071c24553f281d2d5ced92066b24289cf3c2862d
AlexanderVi <cholipso@gmail.com>
2023-12-16 18:42:22 +0300

    Update

---
+++ time.py
@@ -0,0 +1,19 @@
+import time
+start_time = time.time()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"Время выполнения программы: {execution_time} секунд")
